e57e5e5a1d58c017947830c4d07aaee0bd598aea
Issa Mohamed <48192737+issamohamed@users.noreply.github.com>
2022-12-07 20:28:43 -0600

    Update atm_interface.java

--- atm_interface.java
+++ atm_interface.java
@@ -5,7 +5,7 @@ public class ATM {
   public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
 
-    // Initialize balance to $1000
+    // Initializing balance to $1000
     double balance = 1000.0;
 
     // Initialize a boolean for whether the user wants to continue using the ATM
@@ -20,26 +20,26 @@ public class ATM {
       System.out.println("3. Deposit funds");
       System.out.println("4. Exit");
 
-      // Read the user's choice
+      // Reading the user's choice
       int choice = input.nextInt();
 
-      // Perform the selected action
+      // Performing the selected action
       if (choice == 1) {
         // Check balance
         System.out.println("Your balance is: $" + balance);
       } else if (choice == 2) {
-        // Withdraw funds
+        // Withdrawing funds
         System.out.println("Enter amount to withdraw: ");
         double amount = input.nextDouble();
         if (amount > balance) {
-          // Cannot withdraw more than the available balance
+          // if cannot withdraw more than the available balance
           System.out.println("Insufficient funds");
         } else {
           balance -= amount;
           System.out.println("Successfully withdrew $" + amount);
         }
       } else if (choice == 3) {
-        // Deposit funds
+        // Depositing funds
         System.out.println("Enter amount to deposit: ");
         double amount = input.nextDouble();
         balance += amount;
